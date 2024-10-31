d8bf490109eecd2d294d9c127a89936673890100
Eero Prittinen <eero.prittinen@mountkelvin.com>
2024-10-31 10:26:28 +0200

    Update page title

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'Ohjelmointia',
+			title: 'Koodipiiri.fi',
 			social: {
 				github: 'https://github.com/withastro/starlight', // TODO
 			},
